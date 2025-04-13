5f2cfa868df7178f96e66f6248e23488eb44bbcc
Anthony <anthony.sforzin@gmail.com>
2025-04-13 20:05:02 -0300

    Configura pinos dos LEDs, buzzer e sensor LDR

--- vinheria_agnello.ino
+++ vinheria_agnello.ino
@@ -3,6 +3,12 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 2); // Endereço I2C do LCD, 16 colunas e 2 linhas
 
+const int pinoLDR = A0; // Pino analógico para o LDR
+const int ledVerde = 11; // Pino digital para o LED verde
+const int ledAmarelo = 10; // Pino digital para o LED amarelo
+const int ledVermelho = 9; // Pino digital para o LED vermelho
+const int buzzer = 6; // Pino digital para o buzzer
+
 void setup() {
     lcd.init(); // Inicializa o LCD
     lcd.backlight(); // Liga a luz de fundo do LCD
@@ -16,6 +22,11 @@ void setup() {
 
     delay(3000); // Exibe por 3 segundos
     lcd.clear(); // Limpa o LCD
+
+    pinMode(ledVerde, OUTPUT); // Define o pino do LED verde como saída
+    pinMode(ledAmarelo, OUTPUT); // Define o pino do LED amarelo como saída
+    pinMode(ledVermelho, OUTPUT); // Define o pino do LED vermelho como saída
+    pinMode(buzzer, OUTPUT); // Define o pino do buzzer como saída
 }
 
 void loop() {
